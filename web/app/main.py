1bea3faa3c0c193ac04be4650b4749b852ba3a5b
PiatekBartosz <bartekpiatek123@gmail.com>
2024-04-14 17:16:12 +0200

    fastapi: working on application, influx: db settup, not working

--- web/app/main.py
+++ web/app/main.py
@@ -1,5 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
+# test env variables
+import os
 
 app = FastAPI()
 
@@ -10,3 +12,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# Test purposes only
+@app.get("/env")
+def read_root():
+    env = {}
+    env["url"] = os.environ.get("INFLUXDB_API_URL")
+    env["token"] = os.environ.get("INFLUXDB_API_TOKEN")
+    env["org"] = os.environ.get("INFLUX_API_ORGANIZATION")
+    return env
