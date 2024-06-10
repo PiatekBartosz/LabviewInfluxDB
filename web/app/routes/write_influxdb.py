d766d5ea6a6da4e98884088c1f6947e1e8f72e48
PiatekBartosz <bartekpiatek123@gmail.com>
2024-06-10 02:01:31 +0200

    save work

--- web/app/routes/write_influxdb.py
+++ web/app/routes/write_influxdb.py
@@ -4,6 +4,8 @@ from fastapi import APIRouter, Request, Body
 from app.influx_client_wrapper.influx_client_wrapper import InfluxClientWrapper
 from app.influx_client_wrapper.influx_configuration import InfluxConfiguration
 from typing import Any
+from datetime import datetime, timedelta
+import time
 
 write_router = APIRouter()
 
@@ -22,7 +24,7 @@ async def write_api():
 
 
 @write_router.post("/write_api", tags=["write_api"])
-async def write_api_post(payload: Any = Body(None)):
+def write_api_post(payload: Any = Body(None)):
 
     body = payload
 
@@ -36,26 +38,34 @@ async def write_api_post(payload: Any = Body(None)):
     try:
         dict_mesurement = body["Array"][0]
         t0 = dict_mesurement["t0"]
-        measurement = dict_mesurement["y"][0]
+        dt = dict_mesurement["dt"]
         city = payload["City"]
         country = payload["Country"]
         card_id = payload["CardId"]
 
         print("Test write data:")
         print("array = ", dict_mesurement)
-        print("to = ", t0)
-        print("measurement = ", measurement)
+        print("to = ", t0, "type= ", type(t0))
+        print("dt = ", dt, "type= ", type(dt))
         print("city = ", city)
         print("country = ", country)
         print("card_id = ", card_id)
 
-        await client.record_timeseries(
-            measurement=float(measurement),
-            timestamp=t0,
-            city=city,
-            country=country,
-            card_id=int(card_id)
-        )
+        t0 = datetime.fromisoformat(t0.replace('Z', '+00:00'))
+        dt = timedelta(seconds=dt)
+
+        for i, measurement in enumerate(dict_mesurement['y']):
+            timestamp = t0 + i * dt
+            timestamp_string = timestamp.isoformat().replace('+00:00', 'Z')
+            print("timestamp_string: ", timestamp_string)
+            client.record_timeseries(
+                measurement=measurement,
+                timestamp=timestamp_string,
+                city=city,
+                country=country,
+                card_id=str(card_id)
+            )
+
     except Exception as e:
         raise e
 
